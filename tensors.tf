f4fb3fefb6b8d88f42b01c28c51efe699b44071a
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-03-13 11:38:13 +0530

    Update tensors.tf

--- tensors.tf
+++ tensors.tf
@@ -31,3 +31,34 @@ we will see
 
 
 BASIC STRUCTURE :
+
+1. view functions as computational graphs 
+2.  first build a computational graph , then use a session to execute operations in the graph 
+
+ graphs !
+- Nodes are operators (ops) , variables and constants 
+ - edges are tensors
+ - Tensorflow = tensor + flow = data + flow 
+ 
+ 
+ constants : fixed value tensors : not trainable 
+ variabels : tensors initialized in a session : trainable 
+ placeholder : tensors of value that are unknown during graph construction , passed as input during a session
+ Ops : functions as tensors
+ 
+ 
+ y^ = Wx + b ;
+ x : placeholder 
+ W , b : variabels
+ 
+ action between them : ops
+ 
+ 
+ sessions :
+  - sesssions is the runtime environment of a graph ,where operations are executed , and tensors are evaluated 
+ - a.eval() ~ session.run(a)
+ 
+ eval execution of single op and ops that return a value 
+ upon op execution , only subgraph required for calculating its value is evaluated 
+ 
+ 
